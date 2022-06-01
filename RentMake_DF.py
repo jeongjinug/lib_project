7d742c0e1f859ef3f7b14d48ff9cd4c5a5801fe9
aksktlf <aksktlf@gmail.com>
2022-06-01 15:46:17 +0900

    Merge branch 'lib/tool' of https://github.com/jeongjinug/lib_project
    # Conflicts:
    #	tool.py

---
+++ RentMake_DF.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import csv
+
+ProjectRentDf = pd.DataFrame({'Rent_seq' : [], 'Book_ISBN': [], \
+                                        'User_phone': [], 'Rent_Date': [], \
+                                        'Rent_returndate': [], 'Rent_check': []})
+
+ProjectRentDf.set_index('Rent_seq', inplace = True)
+ProjectRentDf.to_csv('RentMake_DF.csv', encoding='utf-8-sig')
